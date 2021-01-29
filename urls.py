d19a6cf3c2b50b31ee998afed7975aad52226120
drxxtc <75825257+drxxtc@users.noreply.github.com>
2021-01-29 16:39:15 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.urls import path
 #from django.contrib.auth import views as views_auth
-from .views import main, getList, ListView, project_list#, FilterView list, project_list
-
+from .views import main, getList, ListView, project_list, proj#, FilterView list, project_list
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   path('main', main, name='main'),
-  path('/list', getList, name='getList'),
+  path('list', getList, name='getList'),
   path('table', ListView.as_view()),
   path('pr_list', project_list),
+  path('proj', proj),
+     #<int: project_id >
+    
+  #path('json/datesfilter', datesfilter)
   #path('filter', FilterView.as_view()),
  # path('myproject', proj, name='myproject')
   #path('list', list, name='list')
@@ -16,3 +21,6 @@ urlpatterns = [
   #path('/filt', getList, name='filt')
   #path('statustime', statustime, statustime)
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
